4e16f10049e9577ae3320cea77b68836c8c23dfe
Eduard-Epurica <epurica.eduard@gmail.com>
2023-10-11 16:59:01 +0300

    Initial Commit

---
+++ commAPI/src/main/java/com/eduard/commAPI/CommApiApplication.java
@@ -0,0 +1,14 @@
+package com.eduard.commAPI;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CommApiApplication {
+
+	public static void main(String[] args) {
+
+		SpringApplication.run(CommApiApplication.class, args);
+	}
+
+}
---
+++ commAPI/src/test/java/com/eduard/commAPI/CommApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.eduard.commAPI;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CommApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
